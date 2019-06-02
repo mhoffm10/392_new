0b2d8e3ae39fe28478e5692af63e897fec3cd2a2
mikej1815 <49454363+mikej1815@users.noreply.github.com>
2019-06-01 19:53:07 -0700

    Update 392_new_project.py

--- 392_new_project.py
+++ 392_new_project.py
@@ -3,7 +3,11 @@ import csv
 
 # Use DictReader
 # It creates an object that puts the values in each row to a key given by an optional parameter for fieldnames
-csv_file = csv.DictReader(open(r"C:\Users\Michael\Downloads\Records.csv"))
+try:
+    csv_file = csv.DictReader(open(r"C:\Users\Michael\Downloads\Records.csv"))
+except FileNotFoundError:
+    print("Wrong file or file path")
+
 
 # Declares some variables to be used for comparison
 max_profit_amount = float(0.00)
@@ -33,44 +37,55 @@ print(f"The max profit is: ", '$' + format(max_profit_amount, ',.2f'))
 print(f"Found in order #:   {num_max_profit}")
 print(f"At index value:     {index_max_profit}\n")
 
-num_min_profit = int(0)
-min_profit_counter = -1
 
 # Reestablish the variable csv_file
 csv_file = csv.DictReader(open(r"C:\Users\Michael\Downloads\Records.csv"))
+
+num_min_profit = int(0)
+min_profit_counter = -1
+formin_max_profit_amount = max_profit_amount
 
 for row in csv_file:
     profit = float(row['Total Profit'])
     min_profit_counter += 1
 
     # Essentially the same as the min_profit_amount if block from above but...
-    # Uses the max_profit_amount from earlier to only change the value if it is smaller than profit being iterated
-    if profit < max_profit_amount:
-        max_profit_amount = profit
+    # Uses the formin_max_profit_amount to only change the value if it is smaller than profit being iterated
+    if profit < formin_max_profit_amount:
+        formin_max_profit_amount = profit
         num_min_profit = row["Order ID"]
         index_min_profit = min_profit_counter
 
 # Below prints out the variables and formats the profit as a currency
-print(f"The min profit is: ", '$' + format(max_profit_amount, ',.2f'))
+print(f"The min profit is: ", '$' + format(formin_max_profit_amount, ',.2f'))
 print(f"Found in order #:   {num_min_profit}")
 print(f"At index value:     {index_min_profit}\n")
+
+
 
 user_input_variable = input("Please enter an Order ID to locate: ")
 
 # Reestablish the variable csv_file
 csv_file = csv.DictReader(open(r"C:\Users\Michael\Downloads\Records.csv"))
 
-# For each in row in csv_file
-# If the row's Order ID matches the user input variable
-# Assigns the current rows Ship Date and Total Revenue to the appropriate variables
-for row in csv_file:
-    if row['Order ID'] == user_input_variable:
-        shipdate_input_variable = row['Ship Date']
-        revenue_input_variable = float(row['Total Revenue'])
+# Tries to execute the following block
+try:
+    # For each in row in csv_file
+    # If the row's Order ID matches the user input variable
+    # Assigns the current rows Ship Date and Total Revenue to the appropriate variables
+    for row in csv_file:
+        if row['Order ID'] == user_input_variable:
+            shipdate_input_variable = row['Ship Date']
+            revenue_input_variable = float(row['Total Revenue'])
 
-# Below prints out the variables and formats the revenue as a currency
-print(f"Order ID:           {user_input_variable}")
-print(f"Ship Date:          {shipdate_input_variable}")
-print("Total Revenue:     ", '$' + format(revenue_input_variable, ',.2f'))
+    # Below prints out the variables and formats the revenue as a currency
+    print(f"Order ID:           {user_input_variable}")
+    print(f"Ship Date:          {shipdate_input_variable}")
+    print("Total Revenue:     ", '$' + format(revenue_input_variable, ',.2f'))
 
-
+# Looks for a NameError since if no Order ID is found it will then attempt
+# to print shipdate_input_variable which will not be assigned if Order Id is wrong
+# Warns the user exception occurs and continues
+except NameError:
+    print("You have entered an invalid Order ID")
+    pass
